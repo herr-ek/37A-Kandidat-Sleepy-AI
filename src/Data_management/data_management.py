6abead1ce59efd46bb79263ed54c3e0aa1eec0f9
Uliinn <isakul@student.chalmers.se>
2026-03-27 14:08:19 +0100

    Dataset distribution generation

--- src/Data_management/data_management.py
+++ src/Data_management/data_management.py
@@ -3,11 +3,18 @@ import numpy as np
 import wfdb
 import pandas as pd
 import os
+from random import shuffle
 
 GAIN = 655.35
 BASELINE = -32768
 FREQ = 200
 
+DATA_LOCATION = "data/processed"
+SET_LOCATION = "set_distribution"
+# Distributions
+TRAIN = 0.8
+TEST = 0.1
+VALIDATE = 0.1
 
 def load_from_mat_and_arousal_to_pandas(file_record) -> pd.DataFrame:
     """
@@ -86,8 +93,46 @@ def load_from_parquet_to_pandas(file_record) -> pd.DataFrame:
     return df
 
 
-if __name__ == "__main__":
 
+def generate_sets(save = True) -> tuple[list[str],list[str],list[str]]:
+  all_dirs = os.listdir(DATA_LOCATION)
+  shuffle(all_dirs)
+
+  n = len(all_dirs)
+
+  training_set = all_dirs[:int(n*TRAIN)]
+  test_set = all_dirs[int(n*TRAIN):int(n*(TRAIN + TEST))]
+  validate_set = all_dirs[int(n*(TRAIN + TEST)):]
+  
+  if save:
+    with open(f"{SET_LOCATION}/training_set.txt","w") as f:
+      for patient in training_set:
+        print(patient,file=f)
+
+    with open(f"{SET_LOCATION}/test_set.txt","w") as f:
+      for patient in test_set:
+        print(patient,file=f)
+
+    with open(f"{SET_LOCATION}/validate_set.txt","w") as f:
+      for patient in validate_set:
+        print(patient,file=f)
+  
+  return training_set, test_set, validate_set
+
+def get_training_test_validate_sets() -> tuple[list[str],list[str],list[str]]:
+  files = ["training_set.txt", "test_set.txt", "validate_set.txt"]
+
+  res = []
+  for file in files:
+    with open(f"{SET_LOCATION}/{file}","r") as f:
+      res.append([v.strip() for v in f.readlines()])
+  
+  return tuple(res)
+
+
+
+
+if __name__ == "__main__":
     # extract_and_save_to_parquet("tr03-0029")
     df = load_from_parquet_to_pandas("tr03-0029")
 
