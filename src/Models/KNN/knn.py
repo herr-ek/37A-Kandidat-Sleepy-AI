8c915b39fdbe97d7aed89a5ce65d05a1cb2d7ebf
Lucas Ekstener <43437993+herr-ek@users.noreply.github.com>
2026-04-16 11:23:12 +0200

    knn update

--- src/Models/KNN/knn.py
+++ src/Models/KNN/knn.py
@@ -12,6 +12,7 @@ except ImportError:
 class KNN(IModel):
     def __init__(self, n_neighbors: int = 5):
         self.model = KNeighborsClassifier(n_neighbors=n_neighbors, weights="balanced")
+        self.weight = 0.5
 
     def train(
         self,
@@ -20,10 +21,16 @@ class KNN(IModel):
         X_val: np.ndarray = None,
         y_val: np.ndarray = None,
     ) -> None:
+        self.weight = len(y_tr[y_tr == 1]) / len(y_tr)
         self.model.fit(X_tr, y_tr)
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        return self.model.predict(X)
+        return np.array(
+            [
+                1 if pred > self.weight else 0
+                for pred in self.model.predict_proba(X)[:, 1]
+            ]
+        )
 
     def save(self, file_path: str) -> None:
         joblib.dump(self.model, file_path)
