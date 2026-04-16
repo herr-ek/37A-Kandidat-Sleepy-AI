61dcbec41f0ec6a203999bfda46b52dfff46defd
Lucas Ekstener <le@ekstener.se>
2026-04-16 11:32:52 +0200

    knn update + result

--- src/Models/KNN/knn.py
+++ src/Models/KNN/knn.py
@@ -11,7 +11,7 @@ except ImportError:
 
 class KNN(IModel):
     def __init__(self, n_neighbors: int = 5):
-        self.model = KNeighborsClassifier(n_neighbors=n_neighbors, weights="balanced")
+        self.model = KNeighborsClassifier(n_neighbors=n_neighbors, weights="distance")
         self.weight = 0.5
 
     def train(
